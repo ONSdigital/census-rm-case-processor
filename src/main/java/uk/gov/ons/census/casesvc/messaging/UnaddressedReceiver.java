c37590fce10395042159242cb76a991ab8a214e0
Nick Grant <41681172+NickGrantONS@users.noreply.github.com>
2019-05-15 16:04:42 +0100

    Rmc 176 uac qid pairs for unaddressed (#6)

---
+++ src/main/java/uk/gov/ons/census/casesvc/messaging/UnaddressedReceiver.java
@@ -0,0 +1,26 @@
+package uk.gov.ons.census.casesvc.messaging;
+
+import org.springframework.integration.annotation.MessageEndpoint;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.transaction.annotation.Transactional;
+import uk.gov.ons.census.casesvc.model.dto.CreateUacQid;
+import uk.gov.ons.census.casesvc.model.entity.UacQidLink;
+import uk.gov.ons.census.casesvc.service.UacProcessor;
+
+@MessageEndpoint
+public class UnaddressedReceiver {
+  private final UacProcessor uacProcessor;
+
+  public UnaddressedReceiver(UacProcessor uacProcessor) {
+    this.uacProcessor = uacProcessor;
+  }
+
+  @Transactional
+  @ServiceActivator(inputChannel = "unaddressedInputChannel")
+  public void receiveMessage(CreateUacQid createUacQid) {
+    UacQidLink uacQidLink =
+        uacProcessor.saveUacQidLink(null, Integer.parseInt(createUacQid.getQuestionnaireType()));
+    uacProcessor.emitUacUpdatedEvent(uacQidLink, null);
+    uacProcessor.logEvent(uacQidLink, "Unaddressed UAC/QID pair created");
+  }
+}
--- src/test/java/uk/gov/ons/census/casesvc/testutil/RabbitQueueHelper.java
+++ src/test/java/uk/gov/ons/census/casesvc/testutil/RabbitQueueHelper.java
@@ -1,7 +1,13 @@
 package uk.gov.ons.census.casesvc.testutil;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
 import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -12,11 +18,13 @@ import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
 import org.springframework.test.context.ActiveProfiles;
+import uk.gov.ons.census.casesvc.model.dto.ResponseManagementEvent;
 
 @Component
 @ActiveProfiles("test")
 @EnableRetry
 public class RabbitQueueHelper {
+  private ObjectMapper objectMapper = new ObjectMapper();
 
   @Autowired private ConnectionFactory connectionFactory;
 
@@ -55,5 +63,16 @@ public class RabbitQueueHelper {
       backoff = @Backoff(delay = 5000))
   public void purgeQueue(String queueName) {
     amqpAdmin.purgeQueue(queueName);
+  }
+
+  public ResponseManagementEvent checkExpectedMessageReceived(BlockingQueue<String> queue)
+      throws IOException, InterruptedException {
+    String actualMessage = queue.poll(20, TimeUnit.SECONDS);
+    assertNotNull("Did not receive message before timeout", actualMessage);
+    ResponseManagementEvent responseManagementEvent =
+        objectMapper.readValue(actualMessage, ResponseManagementEvent.class);
+    assertNotNull(responseManagementEvent);
+    assertEquals("RM", responseManagementEvent.getEvent().getChannel());
+    return responseManagementEvent;
   }
 }
