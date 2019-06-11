a31ff7592eb67a58589fa2be6d02d4455307c1c7
Richard Weeks <richard.weeks@qa.com>
2019-06-11 09:21:56 +0100

    Fix format issues.

--- src/main/java/uk/gov/ons/census/casesvc/model/repository/CaseRepository.java
+++ src/main/java/uk/gov/ons/census/casesvc/model/repository/CaseRepository.java
@@ -3,12 +3,8 @@ package uk.gov.ons.census.casesvc.model.repository;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import uk.gov.ons.census.casesvc.model.entity.Case;
 
 public interface CaseRepository extends JpaRepository<Case, UUID> {
   Optional<Case> findByCaseId(UUID caseId);
-
 }
