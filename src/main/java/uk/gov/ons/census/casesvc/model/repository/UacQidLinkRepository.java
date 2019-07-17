65b0b845a26f56215292c1bb2a186e4e7802aee4
Luke Loze <Luke.Loze@ext.ons.gov.uk>
2019-07-17 09:56:16 +0100

    receipt uses qid

--- src/main/java/uk/gov/ons/census/casesvc/model/repository/UacQidLinkRepository.java
+++ src/main/java/uk/gov/ons/census/casesvc/model/repository/UacQidLinkRepository.java
@@ -1,7 +1,10 @@
 package uk.gov.ons.census.casesvc.model.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import uk.gov.ons.census.casesvc.model.entity.UacQidLink;
 
-public interface UacQidLinkRepository extends JpaRepository<UacQidLink, UUID> {}
+public interface UacQidLinkRepository extends JpaRepository<UacQidLink, UUID> {
+  Optional<UacQidLink> findByQid(String qid);
+}
