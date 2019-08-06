be0be4b19ad114aabce06c7f783fd61b7350bffb
AdeHarrison <adrian.harrison.uk@gmail.com>
2019-08-06 13:49:49 +0100

    fixed RefusalReceiverIT tests

--- src/main/java/uk/gov/ons/census/casesvc/model/dto/RefusalDTO.java
+++ src/main/java/uk/gov/ons/census/casesvc/model/dto/RefusalDTO.java
@@ -1,11 +1,14 @@
 package uk.gov.ons.census.casesvc.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import uk.gov.ons.census.casesvc.type.RefusalType;
 
 @Data
 @NoArgsConstructor
+@JsonInclude(Include.NON_NULL)
 public class RefusalDTO {
   private RefusalType type;
   private String report;
