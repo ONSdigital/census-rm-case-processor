e882eaf5d81fca57988524c1b6dd7e5de0943305
Richard Weeks <richard.weeks@qa.com>
2019-08-23 13:31:42 +0100

    Add event logger.

--- src/main/java/uk/gov/ons/census/casesvc/messaging/CaseEventsAuditReceiver.java
+++ src/main/java/uk/gov/ons/census/casesvc/messaging/CaseEventsAuditReceiver.java
@@ -3,17 +3,24 @@ package uk.gov.ons.census.casesvc.messaging;
 import org.springframework.integration.annotation.MessageEndpoint;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.transaction.annotation.Transactional;
+import uk.gov.ons.census.casesvc.logging.EventLogger;
 import uk.gov.ons.census.casesvc.model.dto.EventTypeDTO;
 import uk.gov.ons.census.casesvc.model.dto.ResponseManagementEvent;
 
 @MessageEndpoint
 public class CaseEventsAuditReceiver {
+
+    private final EventLogger eventLogger;
+
+    public CaseEventsAuditReceiver(EventLogger eventLogger) {
+        this.eventLogger = eventLogger;
+    }
+
     @Transactional
     @ServiceActivator(inputChannel = "surveyLaunchedInputChannel")
     public void receiveMessage(ResponseManagementEvent event) {
         if (event.getEvent().getType() == EventTypeDTO.SURVEY_LAUNCHED) {
-//            eventService.processPrintCaseSelected(event);
-            System.out.println("Survey launched");
+            //eventLogger.logCaseEvent();
         } else {
             throw new RuntimeException(); // Unexpected event type received
         }
