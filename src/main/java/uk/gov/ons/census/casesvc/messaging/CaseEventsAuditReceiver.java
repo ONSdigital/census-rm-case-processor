087f721250545c47342eae1b315dfcd476f99572
Richard Weeks <richard.weeks@qa.com>
2019-08-27 14:13:18 +0100

    Log survey launched event.

--- src/main/java/uk/gov/ons/census/casesvc/messaging/CaseEventsAuditReceiver.java
+++ src/main/java/uk/gov/ons/census/casesvc/messaging/CaseEventsAuditReceiver.java
@@ -4,23 +4,45 @@ import org.springframework.integration.annotation.MessageEndpoint;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.transaction.annotation.Transactional;
 import uk.gov.ons.census.casesvc.logging.EventLogger;
+import uk.gov.ons.census.casesvc.model.dto.EventDTO;
 import uk.gov.ons.census.casesvc.model.dto.EventTypeDTO;
 import uk.gov.ons.census.casesvc.model.dto.ResponseManagementEvent;
+import uk.gov.ons.census.casesvc.model.entity.Case;
+import uk.gov.ons.census.casesvc.model.repository.CaseRepository;
+
+import java.time.OffsetDateTime;
+import java.util.Optional;
+import java.util.UUID;
+
+import static uk.gov.ons.census.casesvc.model.entity.EventType.FULFILMENT_REQUESTED;
+import static uk.gov.ons.census.casesvc.model.entity.EventType.SURVEY_LAUNCED;
+import static uk.gov.ons.census.casesvc.utility.JsonHelper.convertObjectToJson;
 
 @MessageEndpoint
 public class CaseEventsAuditReceiver {
 
     private final EventLogger eventLogger;
 
-    public CaseEventsAuditReceiver(EventLogger eventLogger) {
+    private final CaseRepository caseRepository;
+
+    public CaseEventsAuditReceiver(EventLogger eventLogger, CaseRepository caseRepository) {
         this.eventLogger = eventLogger;
+        this.caseRepository = caseRepository;
     }
 
     @Transactional
     @ServiceActivator(inputChannel = "surveyLaunchedInputChannel")
     public void receiveMessage(ResponseManagementEvent event) {
         if (event.getEvent().getType() == EventTypeDTO.SURVEY_LAUNCHED) {
-            //eventLogger.logCaseEvent();
+            Optional<Case> surveyLaunchedForCase = caseRepository.findByCaseId(UUID.fromString(event.getPayload().getReceipt().getCaseId()));
+            if (surveyLaunchedForCase.isPresent()) {
+                eventLogger.logCaseEvent(
+                        surveyLaunchedForCase.get(),
+                        OffsetDateTime.now(),
+                        "Survey launched",
+                        SURVEY_LAUNCED, event.getEvent(),
+                        convertObjectToJson(event.getPayload()));
+            }
         } else {
             throw new RuntimeException(); // Unexpected event type received
         }
