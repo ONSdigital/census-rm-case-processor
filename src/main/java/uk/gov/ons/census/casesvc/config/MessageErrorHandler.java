5e7cca3b12b3ea4771c388f959b8ee95f60aa651
Nick Grant <nick.grant@ext.ons.gov.uk>
2019-09-12 12:02:45 +0100

    Format code

--- src/main/java/uk/gov/ons/census/casesvc/config/MessageErrorHandler.java
+++ src/main/java/uk/gov/ons/census/casesvc/config/MessageErrorHandler.java
@@ -65,7 +65,7 @@ public class MessageErrorHandler implements ErrorHandler {
     StringBuffer hexString = new StringBuffer();
     for (int i = 0; i < hash.length; i++) {
       String hex = Integer.toHexString(0xff & hash[i]);
-      if(hex.length() == 1) hexString.append('0');
+      if (hex.length() == 1) hexString.append('0');
       hexString.append(hex);
     }
     return hexString.toString();
