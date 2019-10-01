e92b5ff9681e1a2d4f992454e1fca2eead47e1d4
liamtoozer <liam.toozer@gmail.com>
2019-10-01 10:43:43 +0100

    Log event when received

--- src/main/java/uk/gov/ons/census/casesvc/service/CCSPropertyListedService.java
+++ src/main/java/uk/gov/ons/census/casesvc/service/CCSPropertyListedService.java
@@ -1,26 +1,26 @@
 package uk.gov.ons.census.casesvc.service;
 
-import com.godaddy.logging.Logger;
-import com.godaddy.logging.LoggerFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import static uk.gov.ons.census.casesvc.utility.JsonHelper.convertObjectToJson;
+
+import java.time.OffsetDateTime;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import uk.gov.ons.census.casesvc.client.UacQidServiceClient;
 import uk.gov.ons.census.casesvc.logging.EventLogger;
 import uk.gov.ons.census.casesvc.model.dto.CCSPropertyDTO;
 import uk.gov.ons.census.casesvc.model.dto.ResponseManagementEvent;
 import uk.gov.ons.census.casesvc.model.entity.Case;
+import uk.gov.ons.census.casesvc.model.entity.EventType;
+import uk.gov.ons.census.casesvc.model.entity.UacQidLink;
 import uk.gov.ons.census.casesvc.model.repository.UacQidLinkRepository;
 
 @Service
 public class CCSPropertyListedService {
-  private static final Logger log = LoggerFactory.getLogger(CCSPropertyListedService.class);
-  private static final String UAC_UPDATE_ROUTING_KEY = "event.uac.update";
-  private static final String QID_NOT_FOUND_ERROR = "Qid not found error";
+
+  private static final String CCS_ADDRESS_LISTED = "CCS Address Listed";
+  private static final int CCS_INTERVIEWER_HOUSEHOLD_QUESTIONNAIRE_FOR_ENGLAND_AND_WALES = 71;
 
   private final UacQidLinkRepository uacQidLinkRepository;
-  private final RabbitTemplate rabbitTemplate;
-  private final UacQidServiceClient uacQidServiceClient;
+  private final UacService uacService;
   private final EventLogger eventLogger;
   private final CaseService caseService;
 
@@ -32,13 +32,11 @@ public class CCSPropertyListedService {
 
   public CCSPropertyListedService(
       UacQidLinkRepository uacQidLinkRepository,
-      RabbitTemplate rabbitTemplate,
-      UacQidServiceClient uacQidServiceClient,
+      UacService uacService,
       EventLogger eventLogger,
       CaseService caseService) {
-    this.rabbitTemplate = rabbitTemplate;
-    this.uacQidServiceClient = uacQidServiceClient;
     this.uacQidLinkRepository = uacQidLinkRepository;
+    this.uacService = uacService;
     this.eventLogger = eventLogger;
     this.caseService = caseService;
   }
@@ -47,38 +45,28 @@ public class CCSPropertyListedService {
     CCSPropertyDTO ccsProperty = ccsPropertyListedEvent.getPayload().getCcsProperty();
     String caseId = ccsProperty.getCollectionCase().getId();
 
-    Case ccsCase =
+    Case caze =
         caseService.saveCCSCase(
             caseId, ccsProperty.getSampleUnit(), actionPlanId, collectionExerciseId);
-    //    UacDTO uac = questionnaireLinkedEvent.getPayload().getUac();
-    //    String questionnaireId = uac.getQuestionnaireId();
-    //    UacQidLink uacQidLink = uacService.findByQid(questionnaireId);
-    //    Case caze;
-    //
-    //    if (isIndividualQuestionnaireType(questionnaireId)) {
-    //      Case householdCase = caseService.getCaseByCaseId(UUID.fromString(uac.getCaseId()));
-    //      caze = caseService.prepareIndividualResponseCaseFromParentCase(householdCase);
-    //
-    //      caseService.saveAndEmitCaseCreatedEvent(caze);
-    //    } else {
-    //      caze = caseService.getCaseByCaseId(UUID.fromString(uac.getCaseId()));
-    //    }
-    //
-    //    // If UAC/QID has been receipted before case, update case
-    //    if (!uacQidLink.isActive() && !caze.isReceiptReceived()) {
-    //      caze.setReceiptReceived(true);
-    //      caseService.saveAndEmitCaseUpdatedEvent(caze);
-    //    }
-    //
-    //    uacQidLink.setCaze(caze);
-    //    uacService.saveAndEmitUacUpdatedEvent(uacQidLink);
-    //
-    //    eventLogger.logUacQidEvent(
-    //        uacQidLink,
-    //        questionnaireLinkedEvent.getEvent().getDateTime(),
-    //        QUESTIONNAIRE_LINKED,
-    //        EventType.QUESTIONNAIRE_LINKED,
-    //        questionnaireLinkedEvent.getEvent(),
-    //        convertObjectToJson(uac));
+
+    UacQidLink uacQidLink = getNextCCSUacQidLink(caze);
+
+    uacQidLinkRepository.saveAndFlush(uacQidLink);
+
+    eventLogger.logCaseEvent(
+        caze,
+        OffsetDateTime.now(),
+        CCS_ADDRESS_LISTED,
+        EventType.CCS_ADDRESS_LISTED,
+        ccsPropertyListedEvent.getEvent(),
+        convertObjectToJson(ccsProperty));
+  }
+
+  private UacQidLink getNextCCSUacQidLink(Case caze) {
+    UacQidLink uacQidLink =
+        uacService.buildUacQidLink(
+            caze, CCS_INTERVIEWER_HOUSEHOLD_QUESTIONNAIRE_FOR_ENGLAND_AND_WALES);
+    uacQidLink.setCCS(true);
+    return uacQidLink;
   }
 }
