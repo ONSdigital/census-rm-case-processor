43c267843e5d794b0c176877b6504979fe96df6d
Luke Loze <Luke.Loze@ext.ons.gov.uk>
2019-11-15 09:58:55 +0000

    refactor CaseRefGenerator class to use more CONSTANTS

--- src/main/java/uk/gov/ons/census/casesvc/utility/CaseRefGenerator.java
+++ src/main/java/uk/gov/ons/census/casesvc/utility/CaseRefGenerator.java
@@ -7,22 +7,18 @@ import uk.gov.ons.census.casesvc.utility.pseudorandom.FPEException;
 public class CaseRefGenerator {
   private static final int LOWEST_POSSIBLE_CASE_REF = 10000000;
   private static final int HIGHEST_POSSIBLE_CASE_REF = 99999999;
+  // The range of plaintext and ciphertext values - we subtract 1 because 89999999 is prime
+  private static final BigInteger modulus =
+      BigInteger.valueOf(HIGHEST_POSSIBLE_CASE_REF - LOWEST_POSSIBLE_CASE_REF - 1);
+  // A key, that will be used with the HMAC(SHA256) algorithm, note that this is not secure!
+  private static final byte[] hmacKey = new byte[] {0x10, 0x20, 0x10, 0x20, 0x10, 0x20, 0x10, 0x20};
+  private static final FE1 fe1 = new FE1();
+  // An initialisation vector, or tweak, used in the algorithm.
+  private static final byte[] iv = new byte[] {0x01, 0x02, 0x03, 0x04, 0x05, 0x06};
+
 
   public static int getCaseRef(int sequenceNumber) {
-    FE1 fe1 = new FE1();
-
-    // The range of plaintext and ciphertext values - we subtract 1 because 89999999 is prime!!
-    BigInteger modulus =
-        BigInteger.valueOf(HIGHEST_POSSIBLE_CASE_REF - LOWEST_POSSIBLE_CASE_REF - 1);
-
-    // A value to encrypt
     BigInteger plaintextValue = BigInteger.valueOf(sequenceNumber);
-
-    // A key, that will be used with the HMAC(SHA256) algorithm, note that this is not secure!
-    byte[] hmacKey = new byte[] {0x10, 0x20, 0x10, 0x20, 0x10, 0x20, 0x10, 0x20};
-
-    // An initialisation vector, or tweak, used in the algorithm.
-    byte[] iv = new byte[] {0x01, 0x02, 0x03, 0x04, 0x05, 0x06};
 
     try {
       BigInteger encryptedValue = fe1.encrypt(modulus, plaintextValue, hmacKey, iv);
