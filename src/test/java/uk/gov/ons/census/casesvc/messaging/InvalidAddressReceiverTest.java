80453e00fa70921a8f6906a15a76197d341ea9bb
Ryan Grundy <ryan.grundy@ext.ons.gov.uk>
2020-01-08 14:48:07 +0000

    Removed unused mock

--- src/test/java/uk/gov/ons/census/casesvc/messaging/InvalidAddressReceiverTest.java
+++ src/test/java/uk/gov/ons/census/casesvc/messaging/InvalidAddressReceiverTest.java
@@ -1,15 +1,11 @@
 package uk.gov.ons.census.casesvc.messaging;
 
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
-import static uk.gov.ons.census.casesvc.model.entity.EventType.FULFILMENT_REQUESTED;
 import static uk.gov.ons.census.casesvc.testutil.MessageConstructor.constructMessageWithValidTimeStamp;
 
 import org.junit.Test;
-import org.mockito.Mock;
 import org.springframework.messaging.Message;
-import uk.gov.ons.census.casesvc.logging.EventLogger;
 import uk.gov.ons.census.casesvc.model.dto.ResponseManagementEvent;
 import uk.gov.ons.census.casesvc.service.InvalidAddressService;
 import uk.gov.ons.census.casesvc.utility.MsgDateHelper;
@@ -17,10 +13,6 @@ import uk.gov.ons.census.casesvc.utility.MsgDateHelper;
 import java.time.OffsetDateTime;
 
 public class InvalidAddressReceiverTest {
-
-  @Mock
-  private EventLogger eventLogger;
-
 
   @Test
   public void testInvalidAddress() {
