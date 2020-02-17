28adaca2e2c88d7eb05244794b66afd8333732f3
LukeLoze1 <44841627+LukeLoze1@users.noreply.github.com>
2020-02-17 14:00:57 +0000

    CE Receipting (#106)
    * working but not refined
    
    * receipts for CE Ind
    
    * remove unused test and simplify receipting code
    
    * remove debug logging
    
    * change to put Integers to intValue for comparrison

--- src/main/java/uk/gov/ons/census/casesvc/model/repository/CaseRepository.java
+++ src/main/java/uk/gov/ons/census/casesvc/model/repository/CaseRepository.java
@@ -3,10 +3,17 @@ package uk.gov.ons.census.casesvc.model.repository;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import uk.gov.ons.census.casesvc.model.entity.Case;
 
 public interface CaseRepository extends JpaRepository<Case, Integer> {
   Optional<Case> findByCaseId(UUID caseId);
 
   Optional<Case> findByCaseRef(Integer caseRef);
+
+  @Query(
+      value = "SELECT * FROM casev2.cases WHERE case_id =  :caseId FOR UPDATE SKIP LOCKED",
+      nativeQuery = true)
+  Optional<Case> getCaseAndLockByCaseId(@Param("caseId") UUID caseId);
 }
--- src/main/java/uk/gov/ons/census/casesvc/service/CaseReceiptService.java
+++ src/main/java/uk/gov/ons/census/casesvc/service/CaseReceiptService.java
@@ -1,17 +1,22 @@
 package uk.gov.ons.census.casesvc.service;
 
+import static uk.gov.ons.census.casesvc.utility.QuestionnaireTypeHelper.isIndividualQuestionnaireType;
 import static uk.gov.ons.census.casesvc.utility.QuestionnaireTypeHelper.iscontinuationQuestionnaireTypes;
 
+import java.util.Optional;
 import org.springframework.stereotype.Component;
 import uk.gov.ons.census.casesvc.model.entity.Case;
 import uk.gov.ons.census.casesvc.model.entity.UacQidLink;
+import uk.gov.ons.census.casesvc.model.repository.CaseRepository;
 
 @Component
 public class CaseReceiptService {
   private final CaseService caseService;
+  private final CaseRepository caseRepository;
 
-  public CaseReceiptService(CaseService caseService) {
+  public CaseReceiptService(CaseService caseService, CaseRepository caseRepository) {
     this.caseService = caseService;
+    this.caseRepository = caseRepository;
   }
 
   public void receiptCase(UacQidLink uacQidLink) {
@@ -19,9 +24,38 @@ public class CaseReceiptService {
 
     if (caze.isReceiptReceived()) return;
 
-    if (!iscontinuationQuestionnaireTypes(uacQidLink.getQid())) {
-      caze.setReceiptReceived(true);
-      caseService.saveAndEmitCaseUpdatedEvent(caze);
+    if (iscontinuationQuestionnaireTypes(uacQidLink.getQid())) return;
+
+    if (caze.getCaseType().equals("CE") && isIndividualQuestionnaireType(uacQidLink.getQid())) {
+      incrementActualResponseAndSetReceiptedIfAppropriate(caze);
+      return;
     }
+
+    caze.setReceiptReceived(true);
+    caseService.saveAndEmitCaseUpdatedEvent(caze);
+  }
+
+  private void incrementActualResponseAndSetReceiptedIfAppropriate(Case caze) {
+    /*
+      This stops the actualResponses being updated by another thread for another receipt or linking event
+    */
+    Optional<Case> oCase = caseRepository.getCaseAndLockByCaseId(caze.getCaseId());
+
+    if (!oCase.isPresent()) {
+      throw new RuntimeException(
+          "Failed to get row to increment responses, row is probably locked and this should resolve itself: "
+              + caze.getCaseId());
+    }
+
+    Case lockedCase = oCase.get();
+    lockedCase.setCeActualResponses(lockedCase.getCeActualResponses() + 1);
+
+    if (lockedCase.getAddressLevel().equals("U")
+        && lockedCase.getCeActualResponses().intValue()
+            >= lockedCase.getCeExpectedCapacity().intValue()) {
+      lockedCase.setReceiptReceived(true);
+    }
+
+    caseService.saveAndEmitCaseUpdatedEvent(lockedCase);
   }
 }
