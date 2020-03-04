99a4a25d583d5e1f743ee424525b3b84828d773f
liamtoozer <liam.toozer@gmail.com>
2020-03-04 08:47:25 +0000

    Handle multiple QIDs for CCS (#120)

--- src/main/java/uk/gov/ons/census/casesvc/model/dto/CCSPropertyDTO.java
+++ src/main/java/uk/gov/ons/census/casesvc/model/dto/CCSPropertyDTO.java
@@ -1,12 +1,13 @@
 package uk.gov.ons.census.casesvc.model.dto;
 
+import java.util.List;
 import lombok.Data;
 
 @Data
 public class CCSPropertyDTO {
   private CollectionCase collectionCase;
   private SampleUnitDTO sampleUnit;
-  private UacDTO uac;
+  private List<UacDTO> uac;
   private RefusalDTO refusal;
   private InvalidAddress invalidAddress;
 }
