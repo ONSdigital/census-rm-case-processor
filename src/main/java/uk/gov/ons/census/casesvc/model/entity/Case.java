8576d6a00b3e90a71b81bbf9a7e0b009a829b2ff
Hugh Brace <hugh.ons@gmail.com>
2020-04-01 09:47:04 +0100

    remove unnaddressed as delivered flag

--- src/main/java/uk/gov/ons/census/casesvc/model/entity/Case.java
+++ src/main/java/uk/gov/ons/census/casesvc/model/entity/Case.java
@@ -116,9 +116,6 @@ public class Case {
   @Column(nullable = false, columnDefinition = "BOOLEAN DEFAULT false")
   private boolean addressInvalid;
 
-  @Column(nullable = false, columnDefinition = "BOOLEAN DEFAULT false")
-  private boolean undeliveredAsAddressed;
-
   @Column(columnDefinition = "timestamp with time zone")
   @UpdateTimestamp
   private OffsetDateTime lastUpdated;
