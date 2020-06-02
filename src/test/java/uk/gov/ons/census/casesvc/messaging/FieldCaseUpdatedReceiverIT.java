42723c3059699de8fbf5cf505b0aa7f4cf68ae30
Hugh Brace <hugh.ons@gmail.com>
2020-06-02 15:20:25 +0100

    formatting fix

--- src/test/java/uk/gov/ons/census/casesvc/messaging/FieldCaseUpdatedReceiverIT.java
+++ src/test/java/uk/gov/ons/census/casesvc/messaging/FieldCaseUpdatedReceiverIT.java
@@ -22,7 +22,6 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.transaction.annotation.Transactional;
 import uk.gov.ons.census.casesvc.model.dto.*;
 import uk.gov.ons.census.casesvc.model.entity.Case;
 import uk.gov.ons.census.casesvc.model.entity.Event;
