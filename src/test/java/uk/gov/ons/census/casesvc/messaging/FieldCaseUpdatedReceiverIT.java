ebb7cb396c888c9e298371b8b145b43670945443
Hugh Brace <hugh.ons@gmail.com>
2020-06-04 13:37:04 +0100

    add transactional back on

--- src/test/java/uk/gov/ons/census/casesvc/messaging/FieldCaseUpdatedReceiverIT.java
+++ src/test/java/uk/gov/ons/census/casesvc/messaging/FieldCaseUpdatedReceiverIT.java
@@ -22,6 +22,7 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 import uk.gov.ons.census.casesvc.model.dto.*;
 import uk.gov.ons.census.casesvc.model.entity.Case;
 import uk.gov.ons.census.casesvc.model.entity.Event;
@@ -51,6 +52,7 @@ public class FieldCaseUpdatedReceiverIT {
   private String inboundQueue;
 
   @Before
+  @Transactional
   public void setUp() {
     rabbitQueueHelper.purgeQueue(caseUpdatedQueueName);
     rabbitQueueHelper.purgeQueue(inboundQueue);
