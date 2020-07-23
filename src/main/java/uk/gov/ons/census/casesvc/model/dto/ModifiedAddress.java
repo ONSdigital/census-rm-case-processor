f3d55f506f6709c0aa30f54e37dc04a551957ec8
Nick Grant <nick.grant@ext.ons.gov.uk>
2020-07-23 16:53:12 +0100

    Ludicrously unnecessary overcomplexity to satisfy zero requirements except PR review comment

--- src/main/java/uk/gov/ons/census/casesvc/model/dto/ModifiedAddress.java
+++ src/main/java/uk/gov/ons/census/casesvc/model/dto/ModifiedAddress.java
@@ -1,9 +1,12 @@
 package uk.gov.ons.census.casesvc.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import java.util.Optional;
 import lombok.Data;
 
 @Data
+@JsonInclude(Include.NON_ABSENT)
 public class ModifiedAddress {
   private Optional<String> addressLine1;
   private Optional<String> addressLine2;
--- src/main/java/uk/gov/ons/census/casesvc/service/AddressModificationService.java
+++ src/main/java/uk/gov/ons/census/casesvc/service/AddressModificationService.java
@@ -74,28 +74,43 @@ public class AddressModificationService {
 
     Case caze = caseService.getCaseByCaseId(addressModification.getCollectionCase().getId());
 
-    if (addressModification.getNewAddress().getEstabType().isPresent()) {
+    if (addressModification.getNewAddress().getEstabType() != null
+        && addressModification.getNewAddress().getEstabType().isPresent()) {
       caze.setEstabType(addressModification.getNewAddress().getEstabType().get());
     }
 
-    if (addressModification.getNewAddress().getTownName().isPresent()) {
+    if (addressModification.getNewAddress().getTownName() != null
+        && addressModification.getNewAddress().getTownName().isPresent()) {
       caze.setTownName(addressModification.getNewAddress().getTownName().get());
     }
 
-    if (addressModification.getNewAddress().getAddressLine1().isPresent()) {
+    if (addressModification.getNewAddress().getAddressLine1() != null
+        && addressModification.getNewAddress().getAddressLine1().isPresent()) {
       caze.setAddressLine1(addressModification.getNewAddress().getAddressLine1().get());
     }
 
-    if (addressModification.getNewAddress().getAddressLine2().isPresent()) {
-      caze.setAddressLine2(addressModification.getNewAddress().getAddressLine2().get());
+    if (addressModification.getNewAddress().getAddressLine2() != null) {
+      if (addressModification.getNewAddress().getAddressLine2().isPresent()) {
+        caze.setAddressLine2(addressModification.getNewAddress().getAddressLine2().get());
+      } else {
+        caze.setAddressLine2(null);
+      }
     }
 
-    if (addressModification.getNewAddress().getAddressLine3().isPresent()) {
-      caze.setAddressLine3(addressModification.getNewAddress().getAddressLine3().get());
+    if (addressModification.getNewAddress().getAddressLine3() != null) {
+      if (addressModification.getNewAddress().getAddressLine3().isPresent()) {
+        caze.setAddressLine3(addressModification.getNewAddress().getAddressLine3().get());
+      } else {
+        caze.setAddressLine3(null);
+      }
     }
 
-    if (addressModification.getNewAddress().getOrganisationName().isPresent()) {
-      caze.setOrganisationName(addressModification.getNewAddress().getOrganisationName().get());
+    if (addressModification.getNewAddress().getOrganisationName() != null) {
+      if (addressModification.getNewAddress().getOrganisationName().isPresent()) {
+        caze.setOrganisationName(addressModification.getNewAddress().getOrganisationName().get());
+      } else {
+        caze.setOrganisationName(null);
+      }
     }
 
     caseService.saveCaseAndEmitCaseUpdatedEvent(caze, null);
@@ -111,21 +126,36 @@ public class AddressModificationService {
   }
 
   private void validate(AddressModification addressModification) {
-    if (addressModification.getNewAddress().getTownName() == null
-        || (addressModification.getNewAddress().getTownName().isPresent()
-            && StringUtils.isEmpty(addressModification.getNewAddress().getTownName().get()))) {
+    if (addressModification.getNewAddress().getTownName() != null
+        && !addressModification.getNewAddress().getTownName().isPresent()) {
+      throw new RuntimeException("Mandatory town name cannot be set to null");
+    }
+
+    if (addressModification.getNewAddress().getTownName() != null
+        && addressModification.getNewAddress().getTownName().isPresent()
+        && StringUtils.isEmpty(addressModification.getNewAddress().getTownName().get())) {
       throw new RuntimeException("Mandatory town name is empty");
     }
 
-    if (addressModification.getNewAddress().getAddressLine1() == null
-        || (addressModification.getNewAddress().getAddressLine1().isPresent()
-            && StringUtils.isEmpty(addressModification.getNewAddress().getAddressLine1().get()))) {
+    if (addressModification.getNewAddress().getAddressLine1() != null
+        && !addressModification.getNewAddress().getAddressLine1().isPresent()) {
+      throw new RuntimeException("Mandatory address line 1 cannot be set to null");
+    }
+
+    if (addressModification.getNewAddress().getAddressLine1() != null
+        && addressModification.getNewAddress().getAddressLine1().isPresent()
+        && StringUtils.isEmpty(addressModification.getNewAddress().getAddressLine1().get())) {
       throw new RuntimeException("Mandatory address line 1 is empty");
     }
 
-    if (addressModification.getNewAddress().getEstabType() == null
-        || (addressModification.getNewAddress().getEstabType().isPresent()
-            && !ESTAB_TYPES.contains(addressModification.getNewAddress().getEstabType().get()))) {
+    if (addressModification.getNewAddress().getEstabType() != null
+        && !addressModification.getNewAddress().getEstabType().isPresent()) {
+      throw new RuntimeException("Mandatory estab type cannot be set to null");
+    }
+
+    if (addressModification.getNewAddress().getEstabType() != null
+        && addressModification.getNewAddress().getEstabType().isPresent()
+        && !ESTAB_TYPES.contains(addressModification.getNewAddress().getEstabType().get())) {
       throw new RuntimeException("Estab Type not valid");
     }
   }
