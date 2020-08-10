2202eb233206393423003ce90729c0ebfb61f82d
Nick Grant <nick.grant@ext.ons.gov.uk>
2020-08-10 09:19:44 +0100

    Vanquish transaction stuff - it's implicit

--- src/main/java/uk/gov/ons/census/casesvc/config/MessageConsumerConfig.java
+++ src/main/java/uk/gov/ons/census/casesvc/config/MessageConsumerConfig.java
@@ -27,7 +27,6 @@ import uk.gov.ons.census.casesvc.model.dto.ResponseManagementEvent;
 public class MessageConsumerConfig {
   private final ExceptionManagerClient exceptionManagerClient;
   private final ConnectionFactory connectionFactory;
-  private final PlatformTransactionManager transactionManager;
 
   @Value("${messagelogging.logstacktraces}")
   private boolean logStackTraces;
@@ -85,11 +84,9 @@ public class MessageConsumerConfig {
 
   public MessageConsumerConfig(
       ExceptionManagerClient exceptionManagerClient,
-      ConnectionFactory connectionFactory,
-      PlatformTransactionManager transactionManager) {
+      ConnectionFactory connectionFactory) {
     this.exceptionManagerClient = exceptionManagerClient;
     this.connectionFactory = connectionFactory;
-    this.transactionManager = transactionManager;
   }
 
   @Bean
@@ -354,8 +351,6 @@ public class MessageConsumerConfig {
         new SimpleMessageListenerContainer(connectionFactory);
     container.setQueueNames(queueName);
     container.setConcurrentConsumers(consumers);
-    container.setChannelTransacted(true);
-    container.setTransactionManager(transactionManager);
     container.setAdviceChain(retryOperationsInterceptor);
     return container;
   }
