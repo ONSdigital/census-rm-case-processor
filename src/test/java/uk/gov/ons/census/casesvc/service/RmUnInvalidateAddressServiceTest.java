1ef0c34de9f622d8e260cf239ae80f00426cdcce
liamtoozer <liam.toozer@gmail.com>
2020-09-08 09:20:56 +0100

    Tests

---
+++ src/test/java/uk/gov/ons/census/casesvc/service/RmUnInvalidateAddressServiceTest.java
@@ -0,0 +1,88 @@
+package uk.gov.ons.census.casesvc.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static uk.gov.ons.census.casesvc.utility.JsonHelper.convertObjectToJson;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import uk.gov.ons.census.casesvc.logging.EventLogger;
+import uk.gov.ons.census.casesvc.model.dto.ActionInstructionType;
+import uk.gov.ons.census.casesvc.model.dto.EventDTO;
+import uk.gov.ons.census.casesvc.model.dto.EventTypeDTO;
+import uk.gov.ons.census.casesvc.model.dto.Metadata;
+import uk.gov.ons.census.casesvc.model.dto.PayloadDTO;
+import uk.gov.ons.census.casesvc.model.dto.ResponseManagementEvent;
+import uk.gov.ons.census.casesvc.model.dto.RmUnInvalidateAddress;
+import uk.gov.ons.census.casesvc.model.entity.Case;
+import uk.gov.ons.census.casesvc.model.entity.EventType;
+
+@RunWith(MockitoJUnitRunner.class)
+public class RmUnInvalidateAddressServiceTest {
+
+  @Mock private CaseService caseService;
+
+  @Mock private EventLogger eventLogger;
+
+  @InjectMocks private RmUnInvalidateAddressService underTest;
+
+  @Test
+  public void testUnInvalidateAddressHappyPath() {
+    // Given
+    Case caseToUnInvalidate = new Case();
+    UUID expectedCaseId = UUID.randomUUID();
+    caseToUnInvalidate.setCaseId(expectedCaseId);
+    caseToUnInvalidate.setAddressInvalid(true);
+    caseToUnInvalidate.setRegion("E");
+    caseToUnInvalidate.setCaseType("HH");
+    caseToUnInvalidate.setEstabType("HOUSEHOLD");
+
+    ResponseManagementEvent rme = new ResponseManagementEvent();
+
+    EventDTO event = new EventDTO();
+    rme.setEvent(event);
+    event.setDateTime(OffsetDateTime.now());
+    event.setChannel("RM");
+    event.setType(EventTypeDTO.RM_UNINVALIDATE_ADDRESS);
+
+    OffsetDateTime messageTimestamp = OffsetDateTime.now();
+    PayloadDTO payload = new PayloadDTO();
+    rme.setPayload(payload);
+
+    RmUnInvalidateAddress rmUnInvalidateAddress = new RmUnInvalidateAddress();
+    payload.setRmUnInvalidateAddress(rmUnInvalidateAddress);
+    rmUnInvalidateAddress.setCaseId(expectedCaseId);
+
+    when(caseService.getCaseByCaseId(any())).thenReturn(caseToUnInvalidate);
+
+    // When
+    underTest.processMessage(rme, messageTimestamp);
+
+    // Then
+    ArgumentCaptor<Metadata> metadataArgumentCaptor = ArgumentCaptor.forClass(Metadata.class);
+    verify(caseService)
+        .saveCaseAndEmitCaseUpdatedEvent(eq(caseToUnInvalidate), metadataArgumentCaptor.capture());
+    Metadata eventMetadata = metadataArgumentCaptor.getValue();
+    assertThat(eventMetadata.getFieldDecision()).isEqualTo(ActionInstructionType.UPDATE);
+    assertThat(eventMetadata.getCauseEventType()).isEqualTo(EventTypeDTO.RM_UNINVALIDATE_ADDRESS);
+
+    verify(eventLogger)
+        .logCaseEvent(
+            eq(caseToUnInvalidate),
+            any(),
+            eq("Case address un-invalidate"),
+            eq(EventType.RM_UNINVALIDATE_ADDRESS),
+            eq(rme.getEvent()),
+            eq(convertObjectToJson(rmUnInvalidateAddress)),
+            any());
+  }
+}
--- src/test/java/uk/gov/ons/census/casesvc/utility/FieldworkHelperTest.java
+++ src/test/java/uk/gov/ons/census/casesvc/utility/FieldworkHelperTest.java
@@ -4,8 +4,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Test;
 import uk.gov.ons.census.casesvc.model.entity.Case;
+import uk.gov.ons.census.casesvc.model.entity.RefusalType;
 
 public class FieldworkHelperTest {
+
   @Test
   public void testDoNotSendBackToField() {
     assertThat(FieldworkHelper.shouldSendUpdatedCaseToField(null, "FIELD")).isFalse();
@@ -34,6 +36,39 @@ public class FieldworkHelperTest {
     caze.setCaseType("HH");
     caze.setRegion("E");
     caze.setEstabType("MIGRANT WORKERS");
+    assertThat(FieldworkHelper.shouldSendUpdatedCaseToField(caze, "NOT_FIELD")).isFalse();
+  }
+
+  @Test
+  public void testUnInvalidateAddressDoNotSendBackToField() {
+    assertThat(FieldworkHelper.shouldSendUnInvalidatedAddressCaseToField(null, "FIELD")).isFalse();
+  }
+
+  @Test
+  public void testUnInvalidateAddressDoNotSendToNorthernIrelandCommunalEstablishments() {
+    Case caze = new Case();
+    caze.setCaseType("CE");
+    caze.setRegion("N");
+    assertThat(FieldworkHelper.shouldSendUnInvalidatedAddressCaseToField(caze, "NOT_FIELD"))
+        .isFalse();
+  }
+
+  @Test
+  public void testUnInvalidateAddressDoNotSendToRefusedCase() {
+    Case caze = new Case();
+    caze.setCaseType("HH");
+    caze.setRegion("E");
+    caze.setRefusalReceived(RefusalType.HARD_REFUSAL);
+    assertThat(FieldworkHelper.shouldSendUnInvalidatedAddressCaseToField(caze, "NOT_FIELD"))
+        .isFalse();
+  }
+
+  @Test
+  public void testUnInvalidateAddressDoNotSendToTransientPersons() {
+    Case caze = new Case();
+    caze.setCaseType("HH");
+    caze.setRegion("E");
+    caze.setEstabType("TRANSIENT PERSONS");
     assertThat(FieldworkHelper.shouldSendUpdatedCaseToField(caze, "NOT_FIELD")).isFalse();
   }
 }
