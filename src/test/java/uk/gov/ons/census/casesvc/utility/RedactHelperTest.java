8af11f8f240bc678867230e1bc3c4b8e0801eb0d
Nick Grant <nick.grant@ext.ons.gov.uk>
2020-10-22 17:18:13 +0100

    Improve code & write unit test

---
+++ src/test/java/uk/gov/ons/census/casesvc/utility/RedactHelperTest.java
@@ -0,0 +1,32 @@
+package uk.gov.ons.census.casesvc.utility;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import uk.gov.ons.census.casesvc.model.dto.PayloadDTO;
+import uk.gov.ons.census.casesvc.model.dto.ResponseManagementEvent;
+import uk.gov.ons.census.casesvc.model.dto.UacCreatedDTO;
+
+public class RedactHelperTest {
+  @Test
+  public void testRedactWorks() {
+    // GIVEN
+    ResponseManagementEvent rme = new ResponseManagementEvent();
+    PayloadDTO payload = new PayloadDTO();
+    UacCreatedDTO uacCreated = new UacCreatedDTO();
+    uacCreated.setUac("this value needs to be redacted");
+    payload.setUacQidCreated(uacCreated);
+    rme.setPayload(payload);
+
+    // WHEN
+    // Cast is required for the test, but when we use this we only want Object anyway
+    ResponseManagementEvent rmeDeepCopy = (ResponseManagementEvent)RedactHelper.redact(rme);
+
+    // THEN
+    assertThat(rmeDeepCopy.getPayload().getUacQidCreated().getUac()).isEqualTo("XxxxREDACTEDxxxX");
+
+    // Extra check to make sure the original object wasn't accidentally mutated
+    assertThat(rme.getPayload().getUacQidCreated().getUac())
+        .isEqualTo("this value needs to be redacted");
+  }
+}
