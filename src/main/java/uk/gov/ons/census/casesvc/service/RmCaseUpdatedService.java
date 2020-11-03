c8b4bf719c1279d27ff3553a9357f036520eab90
Luke Loze <Luke.Loze@ext.ons.gov.uk>
2020-11-03 08:20:35 +0000

    master merged in

--- src/main/java/uk/gov/ons/census/casesvc/service/RmCaseUpdatedService.java
+++ src/main/java/uk/gov/ons/census/casesvc/service/RmCaseUpdatedService.java
@@ -54,7 +54,7 @@ public class RmCaseUpdatedService {
     caze.setSkeleton(false);
 
     Metadata eventMetadata = null;
-    if (shouldSendCaseToField(caze, rme.getEvent().getChannel())) {
+    if (shouldSendCaseToField(caze)) {
       eventMetadata = new Metadata();
       eventMetadata.setCauseEventType(rme.getEvent().getType());
 
