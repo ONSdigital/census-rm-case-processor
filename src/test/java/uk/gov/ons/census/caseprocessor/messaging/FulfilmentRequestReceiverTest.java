bc70762c60819c0bd98fb325f297d32085b9e8d2
Ravindra Tummuru <Ravindra.Tummuru@ext.ons.gov.uk>
2026-09-08 16:38:58 +0100

    CN-219 Merge from main further formatting, clean up.

--- src/test/java/uk/gov/ons/census/caseprocessor/messaging/FulfilmentRequestReceiverTest.java
+++ src/test/java/uk/gov/ons/census/caseprocessor/messaging/FulfilmentRequestReceiverTest.java
@@ -1,8 +1,8 @@
 package uk.gov.ons.census.caseprocessor.messaging;
 
+import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
